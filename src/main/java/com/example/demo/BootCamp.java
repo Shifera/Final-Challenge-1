cd861cb4348255bb4f3f04c6518e08e1357b3b55
Shifera <shiferawregassa@gmail.com>
2019-12-10 13:22:27 -0500

    2nd commit

--- src/main/java/com/example/demo/BootCamp.java
+++ src/main/java/com/example/demo/BootCamp.java
@@ -1,4 +1,13 @@
 package com.example.demo;
 
-public class BootCamp {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCamp{
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootCamp.class, args);
+    }
+
 }
