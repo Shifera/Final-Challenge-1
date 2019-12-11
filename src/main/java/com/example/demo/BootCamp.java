66d22660b6664c85f17fc9d7bac20bcc4f8bc308
Shifera <shiferawregassa@gmail.com>
2019-12-11 15:18:51 -0500

    yes

--- src/main/java/com/example/demo/BootCamp.java
+++ src/main/java/com/example/demo/BootCamp.java
@@ -1,13 +1,156 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
-@SpringBootApplication
-public class BootCamp{
+@Entity
+public class BootCamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    @Size(min = 2)
+    private String WhichBootCampwouldyouliketoapply;
 
-    public static void main(String[] args) {
-        SpringApplication.run(BootCamp.class, args);
+    @NotNull
+    @Size(min = 2)
+    private String FirstName;
+    @NotNull
+    @Size(min = 2)
+    private String LastName;
+
+    @NotNull
+    @Size(min = 1)
+    private String Email;
+
+    @NotNull
+    @Min(10)
+    private int Phone;
+
+    @NotNull
+    @Min(6)
+    private int DateofBirth;
+
+    @NotNull
+    @Size(min = 1)
+    private boolean WorkPermit;
+
+    @NotNull
+    @Size(min = 1)
+    private boolean Veteran;
+
+    @NotNull
+    @Size(min = 1)
+    private boolean AreYouEmployed;
+
+    @NotNull
+    @Size(min = 1)
+    private String NameofSchool;
+
+    @NotNull
+    @Size(min = 1)
+    private String DegreeEarned;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getWhichBootCampwouldyouliketoapply() {
+        return WhichBootCampwouldyouliketoapply;
+    }
+
+    public void setWhichBootCampwouldyouliketoapply(String whichBootCampwouldyouliketoapply) {
+        WhichBootCampwouldyouliketoapply = whichBootCampwouldyouliketoapply;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(int phone) {
+        Phone = phone;
+    }
+
+    public int getDateofBirth() {
+        return DateofBirth;
+    }
+
+    public void setDateofBirth(int dateofBirth) {
+        DateofBirth = dateofBirth;
+    }
+
+    public boolean isWorkPermit() {
+        return WorkPermit;
+    }
+
+    public void setWorkPermit(boolean workPermit) {
+        WorkPermit = workPermit;
+    }
+
+    public boolean isVeteran() {
+        return Veteran;
+    }
+
+    public void setVeteran(boolean veteran) {
+        Veteran = veteran;
+    }
+
+    public boolean isAreYouEmployed() {
+        return AreYouEmployed;
+    }
+
+    public void setAreYouEmployed(boolean areYouEmployed) {
+        AreYouEmployed = areYouEmployed;
+    }
+
+    public String getNameofSchool() {
+        return NameofSchool;
+    }
+
+    public void setNameofSchool(String nameofSchool) {
+        NameofSchool = nameofSchool;
+    }
+
+    public String getDegreeEarned() {
+        return DegreeEarned;
+    }
+
+    public void setDegreeEarned(String degreeEarned) {
+        DegreeEarned = degreeEarned;
     }
 
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,26 +1,53 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
     public class HomeController {
+    @Autowired
+    BootCampRepository bootCampRepository;
 
-        @RequestMapping("/")
-        public String homePage(){
+            @RequestMapping("/")
+        public String bootlist(Model model){
+            model.addAttribute("BootCamps", bootCampRepository.findAll());
             return "index";
         }
-        @RequestMapping("/author")
-        public String authorPage(){
-            return "author";
-        }
-        @RequestMapping("/java bootcamp")
-        public String javaBootcamp(){
-            return "java bootcamp";
-        }
+    @RequestMapping("/author")
+    public String Author(){
+        return "author";
+    }
+
     @RequestMapping("/apply")
-    public String applyPage(){
+    public String applylist(){
         return "apply";
     }
-    }
+
+        @GetMapping("/add")
+        public String bootCamp(Model model){
+            model.addAttribute("BootCamp", new BootCamp());
+            return "apply";
+        }
+
+
+        @PostMapping("/process")
+        public String processForm(@Valid BootCamp bootCamp, BindingResult result){
+            if (result.hasErrors()){
+                return "apply";
+            }
+            bootCampRepository.save(bootCamp);
+            return "redirect:/";
+        }
+
+        }
+
+
